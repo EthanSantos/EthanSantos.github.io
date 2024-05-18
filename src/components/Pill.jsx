d79eebfd22bd2020137b4b33f60dd540a7f8375c
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-05-17 17:43:45 -0700

    pill hover

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Pill = ({text}) => {
     return (
-        <div className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        <div className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 transform transition-transform duration-300 hover:bg-gray-300 hover:shadow-lg">
             {text}
         </div>
     )
