7aec5b7ff7b80b48f5dfa9c5596cb72bb6efbf4d
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-06-03 16:49:34 -0700

    updated about/experience/resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,25 @@ import Pill from '../components/Pill'
 
 
 const About = () => {
+
+    const information = {
+        About: "I'm a third-year Computer Science major at University of California, Irvine with a deep passion for web development and game development. My programming journey began in late 2016, and since then, I've grown my skills and expertise across various technologies.",
+        Awards: "Best Startup at BearHacks"
+    }
     return (
         <div className="text-gray-900">
             <div className="container mx-auto px-4 py-8">
                 <div className="max-w-2xl mx-auto">
                     <h1 className="text-2xl font-bold mb-4">About Me</h1>
-                    <p className="mb-4 text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa id neque. Ut lectus arcu bibendum at varius vel pharetra. Nec nam aliquam sem et tortor consequat. Aliquam eleifend mi in nulla posuere sollicitudin aliquam. Dictum non consectetur a erat nam at lectus urna duis. At tellus at urna condimentum mattis pellentesque id nibh tortor. Volutpat diam ut venenatis tellus in metus vulputate eu. Nibh nisl condimentum id venenatis a condimentum vitae sapien. At risus viverra adipiscing at in tellus integer feugiat. Vivamus at augue eget arcu dictum varius duis at consectetur. Ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                    <p className="mb-4 text-base text-gray-600">{information.About}</p>
+                    <div>
+                        <h2 className="text-lg font-semibold">Game Development</h2>
+                        <p className="mb-4 text-base text-gray-600">I started developing games on the ROBLOX platform and created my first successful game during quarantine in 2020 called AIMSTARS. I am proud to share that the game has amassed over 9 million plays and generated over $30,000 in revenue. AIMSTARS still has 15,000 monthly active users today!</p>
+                    </div>
+                    <div>
+                        <h2 className="text-lg font-semibold">Web Development</h2>
+                        <p className="mb-8 text-base text-gray-600">I also have a lot of experience in web development, participating in multiple hackathons, where I currently have 3 wins under my belt. These experiences have sharpened my skills and fostered my creativity in building innovative projects.</p>
+                    </div>
                     <h1 className="text-2xl font-bold mb-4">Education</h1>
                     <div className="flex justify-between">
                         <div>
@@ -26,14 +39,21 @@ const About = () => {
                         </div>
                         <p className="text-base text-gray-600">Aug. 2021 – June 2023</p>
                     </div>
+                    <h1 className="text-2xl font-bold mb-4">Awards</h1>
+                    <p className="mb-2 text-base text-gray-600">{information.Awards}</p>
                     <h1 className="text-2xl font-bold mb-4">Skills</h1>
                     <Pill text={"C++"} />
                     <Pill text={"Java"} />
                     <Pill text={"JavaScript"} />
+                    <Pill text={"TypeScript"} />
                     <Pill text={"Python"} />
                     <Pill text={"Lua"} />
                     <Pill text={"SQL"} />
                     <Pill text={"HTML/CSS"} />
+                    <Pill text={"React"} />
+                    <Pill text={"Flask"} />
+                    <Pill text={"TailwindCSS"} />
+                    <Pill text={"MaterialUI"} />
                 </div>
             </div>
         </div>
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -7,28 +7,54 @@ const Experience = () => {
             <div className="container mx-auto px-4 py-8">
                 <div className="max-w-2xl mx-auto">
                     <h1 className="text-2xl font-bold mb-4">Work Experience</h1>
-                    <div className="flex justify-between">
+
+                    <div className="flex justify-between items-start mb-8">
                         <div>
-                            <h2 className="text-lg font-semibold">Game Developer</h2>
-                            <p className="text-base">Buzzy.gg</p>
-                            <p className="mb-4 text-base text-gray-500">Description</p>
+                            <div className="flex justify-between items-baseline w-full">
+                                <h2 className="text-lg font-semibold">Game Developer</h2>
+                                <p className="text-base">Mar. 2024 – Present</p>
+                            </div>
+                            <p className="mb-2 text-base">Buzzy.gg</p>
+                            <p className="mb-4 text-base text-gray-500">
+                                I developed a game builder that significantly impacted thousands of students in classrooms through CodeCamp. I created a global server search system using TeleportService, which enabled players to join any user-created game instance seamlessly. Additionally, I integrated HTTP JSON protocols to send data about players’ games to a reserve server, enhancing data accuracy by 20%. I also established and configured a comprehensive database system to track likes, allowing players to rate user-created games effectively.
+                            </p>
                             <Pill text={"Lua"} />
                             <Pill text={"Roblox Studio"} />
                             <Pill text={"Git"} />
                         </div>
-                        <p className="text-base text-gray-600">Mar. 2024 – Present</p>
                     </div>
-                    <div className="flex justify-between">
+
+                    <div className="flex justify-between items-start mb-8">
                         <div>
-                            <h2 className="text-lg font-semibold">Math Tutor</h2>
-                            <p className="text-base">Mount San Antonio College</p>
-                            <p className="mb-4 text-base text-gray-500">Description</p>
+                            <div className="flex justify-between items-baseline w-full">
+                                <h2 className="text-lg font-semibold">Founder/Developer</h2>
+                                <p className="text-base">Jun. 2020 – Present</p>
+                            </div>
+                            <p className="mb-2 text-base">AIMSTARS</p>
+                            <p className="mb-4 text-base text-gray-500">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa id neque. Ut lectus arcu bibendum at varius vel pharetra. Nec nam aliquam sem et tortor consequat. Aliquam eleifend mi in nulla posuere sollicitudin aliquam. Dictum non consectetur a erat nam at lectus urna duis. At tellus at urna condimentum mattis pellentesque id nibh tortor. Volutpat diam ut venenatis tellus in metus vulputate eu. Nibh nisl condimentum id venenatis a condimentum vitae sapien. At risus viverra adipiscing at in tellus integer feugiat. Vivamus at augue eget arcu dictum varius duis at consectetur. Ac feugiat sed lectus vestibulum mattis ullamcorper velit.
+                            </p>
+                            <Pill text={"Lua"} />
+                            <Pill text={"Roblox Studio"} />
+                            <Pill text={"Git"} />
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between items-start mb-8">
+                        <div>
+                            <div className="flex justify-between items-baseline w-full">
+                                <h2 className="text-lg font-semibold">Math Tutor</h2>
+                                <p className="text-base">Aug. 2021 – June 2023</p>
+                            </div>
+                            <p className="mb-2 text-base">Mount San Antonio College</p>
+                            <p className="mb-4 text-base text-gray-500">
+                                As a dedicated Math Tutor, I have extensive experience in both drop-in tutoring and personalized one-on-one appointments. I was assigned to work closely with students for an entire semester, providing tutoring sessions that lasted up to 2-3 hours. My teaching covered subjects such as Calculus, Pre-Calculus, and Trigonometry. To enhance the students' learning experience, I created personalized problems tailored to their specific needs. I also carefully communicated with students to ensure the best possible learning outcomes, demonstrating patience and addressing any challenges that arose during tutoring sessions.
+                            </p>
                             <Pill text={"Calculus"} />
-                            <Pill text={"Algebra"} />
+                            <Pill text={"Pre-Calculus"} />
                             <Pill text={"Trigonometry"} />
                             <Pill text={"Teaching"} />
                         </div>
-                        <p className="text-base text-gray-600">Aug. 2021 – June 2023</p>
                     </div>
                 </div>
             </div>
