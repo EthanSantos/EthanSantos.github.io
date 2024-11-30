0bf9ec0faede7ab549306d0453add3a1620c863b
EthanSantos <ethanangelo61@gmail.com>
2024-11-29 21:05:04 -0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
     return (
         <footer className="py-4 text-center">
-            <div className="container mx-auto">
-                <p className="text-sm">
-                    Copyright © {new Date().getFullYear()} Ethan Santos. All Rights Reserved.
+            <div className="container mx-auto max-w-xlg">
+                <div className="border-t border-gray-300 mb-4"></div>
+                <p className="text-sm text-gray-500">
+                    COPYRIGHT © {new Date().getFullYear()} ETHAN SANTOS. ALL RIGHTS RESERVED.
                 </p>
             </div>
         </footer>
     );
-}
+};
 
-export default Footer
+export default Footer;
