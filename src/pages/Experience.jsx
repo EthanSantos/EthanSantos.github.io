230f0a63d13473b1010fcd83291119948678884b
EthanSantos <ethanangelo61@gmail.com>
2025-01-20 14:49:56 -0800

    responsive experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -7,22 +7,22 @@ import { experiences } from '../components/ExperiencesData';
 const Experience = () => {
     return (
         <div className="min-h-screen bg-white text-black py-12">
-            <div className="container mx-auto px-4 md:px-48">
+            <div className="container mx-auto px-4 lg:px-48">
                 <div className="relative">
                     {/* Timeline vertical line (desktop) */}
-                    <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2 h-full border-l-2 border-gray-300"></div>
+                    <div className="hidden lg:block absolute left-1/2 transform -translate-x-1/2 h-full border-l-2 border-gray-300"></div>
 
-                    {/* Timeline vertical line (mobile) */}
-                    <div className="md:hidden absolute left-8 h-full border-l-2 border-gray-300"></div>
+                    {/* Timeline vertical line (mobile/tablet) */}
+                    <div className="lg:hidden absolute left-8 h-full border-l-2 border-gray-300"></div>
 
                     {experiences.map((exp, index) => (
                         <div
                             key={index}
-                            className="mb-8 flex justify-between items-start w-full md:flex-row flex-col md:relative"
+                            className="mb-8 flex justify-between items-start w-full lg:flex-row flex-col lg:relative"
                         >
                             {/* Logo circle (desktop) */}
                             <div
-                                className="hidden md:flex absolute left-1/2 transform -translate-x-1/2 items-center justify-center w-20 h-20 rounded-full bg-white border-2 border-gray-300 z-10"
+                                className="hidden lg:flex absolute left-1/2 transform -translate-x-1/2 items-center justify-center w-20 h-20 rounded-full bg-white border-2 border-gray-300 z-10"
                                 style={{ top: '50%', transform: 'translate(-50%, -50%)' }}
                             >
                                 <img
@@ -32,9 +32,9 @@ const Experience = () => {
                                 />
                             </div>
 
-                            {/* Mobile timeline layout */}
-                            <div className="md:hidden w-full flex">
-                                {/* Logo circle (mobile) */}
+                            {/* Mobile/Tablet timeline layout */}
+                            <div className="lg:hidden w-full flex">
+                                {/* Logo circle (mobile/tablet) */}
                                 <div className="flex-shrink-0 w-16 h-16 rounded-full bg-white border-2 border-gray-300 z-10 relative left-0">
                                     <img
                                         src={exp.logo}
@@ -43,13 +43,18 @@ const Experience = () => {
                                     />
                                 </div>
 
-                                {/* Content card (mobile) */}
+                                {/* Content card (mobile/tablet) */}
                                 <motion.div
-                                    className="flex-grow ml-4 bg-gray-100 p-6 rounded-lg shadow-lg border border-gray-300"
+                                    className="flex-grow ml-4 bg-gray-100 p-6 rounded-lg shadow-lg border border-gray-300 relative"
                                     initial={{ opacity: 0, y: 50 }}
                                     animate={{ opacity: 1, y: 0 }}
                                     transition={{ duration: 0.5, delay: index * 0.2 }}
                                 >
+                                    {/* Arrow pointing to logo */}
+                                    <div className="absolute left-0 top-[32px] transform -translate-x-full">
+                                        <div className="w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-r-8 border-r-gray-300"></div>
+                                    </div>
+
                                     <h2 className="text-lg font-bold">{exp.title}</h2>
                                     <h3 className="text-sm text-gray-500">{exp.company}</h3>
                                     <p className="text-xs text-gray-400">{exp.date}</p>
@@ -68,7 +73,7 @@ const Experience = () => {
                             {index % 2 === 0 ? (
                                 <>
                                     {/* Right-side card (desktop) */}
-                                    <div className="hidden md:flex md:w-[45%] justify-end md:pr-2 relative">
+                                    <div className="hidden lg:flex lg:w-[45%] justify-end lg:pr-2 relative">
                                         <motion.div
                                             className="bg-gray-100 p-6 rounded-lg shadow-lg border border-gray-300 w-full"
                                             initial={{ opacity: 0, y: 50 }}
@@ -93,13 +98,13 @@ const Experience = () => {
                                             </div>
                                         </motion.div>
                                     </div>
-                                    <div className="hidden md:block w-[45%]"></div>
+                                    <div className="hidden lg:block w-[45%]"></div>
                                 </>
                             ) : (
                                 <>
-                                    <div className="hidden md:block w-[45%]"></div>
+                                    <div className="hidden lg:block w-[45%]"></div>
                                     {/* Left-side card (desktop) */}
-                                    <div className="hidden md:flex md:w-[45%] justify-start md:pl-2 relative">
+                                    <div className="hidden lg:flex lg:w-[45%] justify-start lg:pl-2 relative">
                                         <motion.div
                                             className="bg-gray-100 p-6 rounded-lg shadow-lg border border-gray-300 w-full"
                                             initial={{ opacity: 0, y: 50 }}
