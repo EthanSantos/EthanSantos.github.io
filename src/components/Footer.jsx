16b0bdfb5b7c1dbe0f73598944f185565621a00b
EthanSantos <ethanangelo61@gmail.com>
2025-02-08 21:58:53 -0800

    better footer and responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="py-4 text-center">
-            <div className="container mx-auto max-w-xlg">
+        <footer className="py-4">
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="border-t border-gray-300 mb-4"></div>
-                <p className="text-sm text-gray-500">
-                    COPYRIGHT © {new Date().getFullYear()} ETHAN SANTOS. ALL RIGHTS RESERVED.
-                </p>
-                <p className="text-xs text-gray-400 mt-2">
-                    Built with React, TailwindCSS and ❤️
-                </p>
+                <div className="flex flex-col md:flex-row justify-between items-center">
+                    <p className="text-sm text-gray-500">
+                        COPYRIGHT © {new Date().getFullYear()} ETHAN SANTOS. ALL RIGHTS RESERVED.
+                    </p>
+                    <p className="text-xs text-gray-400 mt-2 md:mt-0">
+                        Built with React, TailwindCSS and ❤️
+                    </p>
+                </div>
             </div>
         </footer>
     );
